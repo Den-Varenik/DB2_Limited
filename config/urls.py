bc4f830c7136697ffc3e058a2a2bd03d8ad8b61b
Den_Varenik <varenik.denisua@gmail.com>
2021-10-14 16:20:52 +0300

    Start project

---
+++ config/urls.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+from django.conf.urls import url, include
+from django.contrib import admin
+
+urlpatterns = [
+    # Django Admin, use {% url 'admin:index' %}
+    url(settings.ADMIN_URL, admin.site.urls),
+]
